cdd4263385009b9e3788c8124131720c442bb3a6
Pnevmonidis Alexandros <phonesere@gmail.com>
2023-01-06 18:59:05 +0200

    Some changes

--- www/bluff.php
+++ www/bluff.php
@@ -2,9 +2,11 @@
 
 require_once "../lib/dbconnect.php";
 require_once "../lib/table.php";
+require_once "../lib/game.php";
+
 
 $method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 
 // $request = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
 // Σε περίπτωση που τρέχουμε php –S
@@ -19,46 +21,47 @@ switch ($r = array_shift($request)) {
                 handle_table($method);
                 break;
             case 'piece':
-               handle_piece($method, $request[0], $request[1], $input);
+                handle_piece($method, $request[0], $request[1], $input);
                 break;
             case 'player':
-               handle_player($method, $request[0], $input);
+                handle_player($method, $request[0], $input);
                 break;
             default:
                 header("HTTP/1.1 404 Not Found");
                 break;
         }
         break;
-    case 'status': 
-			if(sizeof($request)==0) {handle_status($method);}
-			else {header("HTTP/1.1 404 Not Found");}
-			break;
-	case 'players': handle_player($method, $request,$input);
-			    break;
-	default:  header("HTTP/1.1 404 Not Found");
-                        exit;
+    case 'status':
+        if (sizeof($request) == 0) {
+            handle_status($method);
+        } else {
+            header("HTTP/1.1 404 Not Found");
+        }
+        break;
+    case 'players':
+        handle_player($method, $request, $input);
+        break;
     default:
         header("HTTP/1.1 404 Not Found");
         exit;
 }
 
 
-function handle_table($method) {
-    if($method=='GET') {
-            show_table();
-    } else if ($method=='POST') {
-            reset_table();
+function handle_table($method)
+{
+    if ($method == 'GET') {
+        show_status();
     } else {
         header('HTTP/1.1405 Method Not Allowed');
-    }    
+    }
 }
 
-function handle_piece($method, $x,$y,$input) {
-    ;
+function handle_piece($method, $x, $y, $input)
+{;
 }
 
-function handle_player($method, $p,$input) {
-    ;
+function handle_player($method, $p, $input)
+{;
 }
 
 function handle_status($method)
@@ -69,5 +72,3 @@ function handle_status($method)
         header('HTTP/1.1 405 Method Not Allowed');
     }
 }
-
-?>
